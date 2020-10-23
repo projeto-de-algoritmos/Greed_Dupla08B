86d58f2131ca4c6f19a68029c335c3b8ff1c70a7
Arthur <arthurarp@hotmail.com>
2020-10-23 13:17:43 -0300

    adicionando funcoes para completar informaçoes do agendamento

--- agendamento.js
+++ agendamento.js
@@ -9,6 +9,35 @@ class Agendamento {
         console.log(this.dados);
     }
 
+    minutosParaHoras(minutos) {
+        let horas = Math.floor(minutos / 60);
+        let sobra = minutos % 60;
+
+        if(horas < 10 && sobra < 10)
+            return "0" + horas.toString() + ":0" + sobra.toString();
+        if(horas < 10 && sobra >= 10)
+            return "0" + horas.toString() + ":" + sobra.toString();
+        if(horas >= 10 && sobra < 10)
+            return horas.toString() + ":0" + sobra.toString();
+        if(horas >= 10 && sobra >= 10)
+            return horas.toString() + ":" + sobra.toString();
+    }
+
+    tipoDeServico(nome_do_servico) {
+        if(nome_do_servico === "Trocar o roteador")
+            return 15;
+        if(nome_do_servico === "Trocar antena da TV")
+            return 30;
+        if(nome_do_servico === "Instalar ponto adicional")
+            return 45;
+        if(nome_do_servico === "Cancelamento do serviço")
+            return 60;
+        if(nome_do_servico === "Resolver problema da internet")
+            return 90;
+        if(nome_do_servico === "Trocar toda a fiação")
+            return 120;
+    }
+
     melhor_agendamento() {
         this.dados.sort(function(a,b) {
             return a.em_casa_ate < b.em_casa_ate ? -1 : a.em_casa_ate > b.em_casa_ate ? 1 : 0;
